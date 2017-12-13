76272c5e0ba694a5bf4535a6545ccdcd87862521
itbaiyang <459676797@qq.com>
2017-12-13 18:09:58 +0800

    qwe

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const toolkit = require('./toolkit');
+const matrix = toolkit.makeMatrix();
+
+console.log(matrix);
+
+const a = Array.from({ length: 9 }, (v, i) => i);
+console.log(a);
+
+console.log(toolkit.shuffle(a));
